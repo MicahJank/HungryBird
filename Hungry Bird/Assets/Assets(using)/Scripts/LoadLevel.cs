011c58637dd1c2bbdec702bb9081d5aedddae8da
MicahJank <MicahJank@users.noreply.github.com>
2019-01-22 14:43:00 -0600

    Hungry Bird 0.03
    Implemented basic splash screen and music.

---
+++ Hungry Bird/Assets/Assets(using)/Scripts/LoadLevel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       Invoke("Load", 2f);
+    }
+
+    private void Load()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
