4b6e253889231ee7c19bf608319f2bfc25bf3666
MicahJank <MicahJank@users.noreply.github.com>
2019-01-22 21:04:06 -0600

    Hungry Bird 0.04
    Implemented waypoint system.

---
+++ Hungry Bird/Assets/Assets(using)/Scripts/DontDestroy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    private string _name;
+    private List<string> _objectNames = new List<string>();
+
+    private void Awake()
+    {
+        FindDuplicatedObject();
+        CheckListSize();
+    }
+
+    private void FindDuplicatedObject()
+    {
+        _name = this.gameObject.name;
+        GameObject[] objects = GameObject.FindGameObjectsWithTag(this.tag);
+        foreach (GameObject obj in objects)
+        {
+            if (obj.name == this.name)
+            {
+                _objectNames.Add(obj.name); // Duplicated objs get moved into the list
+            }
+        }
+    }
+
+    private void CheckListSize()
+    {
+        if (_objectNames.Count > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
+
