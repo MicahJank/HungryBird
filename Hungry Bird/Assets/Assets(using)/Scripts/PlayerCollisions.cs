225aae72ae76d27e48d6deaa82996adacac1e876
MicahJank <MicahJank@users.noreply.github.com>
2019-01-28 22:33:58 -0600

    Hungry Bird 0.21
    Added explosion FX when player dies as well as implemented a reload of the current scene on death.

--- Hungry Bird/Assets/Assets(using)/Scripts/PlayerCollisions.cs
+++ Hungry Bird/Assets/Assets(using)/Scripts/PlayerCollisions.cs
@@ -2,18 +2,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Only ok as long as this is the only script that loads scenes
 
 public class PlayerCollisions : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 3f;
+
+    [Tooltip("FX prefab on player")][SerializeField] GameObject deathFX;
 
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        BeginExplosion();
+        Invoke("LoadCurrentScene", levelLoadDelay);
+
     }
 
     private void StartDeathSequence()
     {
-        Debug.Log("Player is dying!");
         SendMessage("DisableControls");
+       
+    }
+
+    private void BeginExplosion()
+    {
+        deathFX.SetActive(true);
+    }
+
+   private void LoadCurrentScene() // String Reference
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
     }
 } 
