a010818f681f4363ef7c5389a6bbac8a74a13662
MicahJank <MicahJank@users.noreply.github.com>
2019-01-29 23:07:38 -0600

    Hungry Bird 0.23
    Nested the enemy explosion in their own prefab at runtime to keep things organized.

--- Hungry Bird/Assets/Assets(using)/Scripts/Enemy.cs
+++ Hungry Bird/Assets/Assets(using)/Scripts/Enemy.cs
@@ -1,21 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [Tooltip("FX prefab on enemy")] [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = false;
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+        boxCollider.size = new Vector3(1.5f, 1.5f, 3.8f);
         
     }
 
     private void OnParticleCollision(GameObject other)
     {
         Debug.Log("Particles Collided with Enemy: " + gameObject.name);
-        Instantiate(deathFX, gameObject.transform.position, Quaternion.identity);
+        GameObject fx = Instantiate(deathFX, gameObject.transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         DestroyEnemy();
         
     }
